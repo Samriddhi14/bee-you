6428b7caa241731c7ff6f017a54c31c3ddf66528
Samriddhi14 <sam14.diva@gmail.com>
2024-07-15 13:21:22 +0530

    Add existing project files to Git

---
+++ frontend/src/components/svgs/X.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../../assets/logo.png'; // Adjust the path as necessary
+
+const XSvg = (props) => (
+  <img src={logo} alt="BeeYou Logo" {...props} />
+);
+
+export default XSvg;
